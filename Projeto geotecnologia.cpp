b67f005c8a1ab00695158ac99ba64422372c922c
Arthur Catarino <155127382+ArthurCatarino@users.noreply.github.com>
2025-05-11 19:32:09 -0300

    Update Projeto geotecnologia.cpp

--- Projeto geotecnologia.cpp
+++ Projeto geotecnologia.cpp
@@ -1,42 +1,91 @@
 #include <iostream>
-#include <unordered_map>
-#include <vector>
+#include <stack>
 #include <utility>
+#include <vector>
+#include <unordered_map>
 #include <fstream>
-#include <string>
-#include <sstream>
+#include <iomanip>
+#include<sstream>
 
 using namespace std;
 
-typedef unordered_map<long long, pair<double, double>> no;
-typedef unordered_map<long long, vector<long long>> g;
+typedef unordered_map<long long, pair<double,double>> no; // pair de coordenadas lat e long
+typedef unordered_map<long long, vector<pair<int,long long>>> g; // sendo o pair do vector {id da aresta , destino}
+typedef unordered_map<long long,int> visitado;
 
-void exemplo_map() {
-	unordered_map <int, vector<string>> map;
+bool busca_visita(long long origem,g &grafo, pair<long long, long long> destino, visitado &info) {
+	
+	info[origem] = 2;
 
-	map[1].push_back({ "Ola" });
-	map[1].push_back({ "Mundo" });
-	map[1].push_back({ "!" });
-
-	for (const auto& par : map) {
-		cout << "Chave : " << par.first;
-		for (auto i : par.second) {
-			cout << " " << i;
+	for (auto i : grafo[origem]) {
+		if (info[i.second] == 1 and i.first != destino.first) {
+			if (i.second == destino.second) {
+				return true;
+			}
+			if (busca_visita(i.second, grafo, destino, info)) {
+				return true;
+			}
 		}
 	}
+	info[origem] = 3;
+	return false;
+
+}
+
+bool busca_em_profundidade(no &nos, g &grafo, long long origem, pair<long long,long long> destino) {
+
+	visitado info;
+
+	for (auto &i : nos) {
+		info[i.first] = 1;
+	}
+
+	info[origem] = 2;
+
+	for (auto& i: grafo[origem]) {
+		if (i.first != destino.first) {
+			if (busca_visita(i.second, grafo, destino, info)) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+vector<pair<long long, long long>> verifica_arestas_criticas(no& nos, g& grafo) {
+
+	vector<pair<long long, long long>> arestas_criticas;
+
+	int p = 0;
+	int tamanho = grafo.size();
+	int intervalos = tamanho / 20;
+
+	for (auto origem : grafo) {
+		p++;
+		if (p % intervalos  == 0) {
+			cout << "(" << p << "/" << tamanho << ")" << endl;
+		}
+		for (auto destino : origem.second) {
+			if (!busca_em_profundidade(nos,grafo, origem.first, destino)) {
+				arestas_criticas.push_back({ origem.first,destino.second });
+			}
+		}
+	}
+	return arestas_criticas;
 }
 
 void importa_grafo(no& nos) {
-	
-	ifstream arquivo("C:\\Users\\arthu\\OneDrive\\Área de Trabalho\\Programaçao\\Geoloc\\Grafo\\nos.csv");
+
+	ifstream arquivo("C:\\Users\\arthu\\OneDrive\\Ãrea de Trabalho\\ProgramaÃ§ao\\Geoloc\\Grafo\\nos.csv");
 
 	if (!arquivo) {
-		cout << "Ãrquivo dos NOS não foi aberto";
+		cout << "Ãƒrquivo dos NOS nÃ£o foi aberto";
 		return;
 	}
 
 	string linha, valor;
-	long long id, x, y;
+	long long id;
+	double x, y;
 
 	getline(arquivo, linha);
 
@@ -47,10 +96,11 @@ void importa_grafo(no& nos) {
 		id = stoll(valor);
 
 		getline(ss, valor, ',');
-		x = stoll(valor);
+		x = stold(valor);
+
 
 		getline(ss, valor, ',');
-		y = stoll(valor);
+		y = stold(valor);
 
 		nos[id] = { x,y };
 	}
@@ -59,15 +109,16 @@ void importa_grafo(no& nos) {
 
 void importa_arestas(g& grafo) {
 
-	ifstream arquivo("C:\\Users\\arthu\\OneDrive\\Área de Trabalho\\Programaçao\\Geoloc\\Grafo\\arestas.csv");
+	ifstream arquivo("C:\\Users\\arthu\\OneDrive\\Ãrea de Trabalho\\ProgramaÃ§ao\\Geoloc\\Grafo\\arestas.csv");
 
 	if (!arquivo) {
-		cout << "Ãrquivo das arestas não foi aberto";
+		cout << "Ãƒrquivo das arestas nÃ£o foi aberto";
 		return;
 	}
 
 	string linha, valor;
 	long long origem, destino;
+	int id;
 
 	getline(arquivo, linha);
 
@@ -76,25 +127,37 @@ void importa_arestas(g& grafo) {
 		stringstream ss(linha);
 
 		getline(ss, valor, ',');
+		id = stoi(valor);
+
+		getline(ss, valor, ',');
 		origem = stoll(valor);
 
 		getline(ss, valor, ',');
 		destino = stoll(valor);
 
-		grafo[origem].push_back(destino);
+		grafo[origem].push_back({ id,destino });
 	}
 
 	arquivo.close();
-	
+
 }
 
 int main() {
 	
 	no nos;
 	g grafo;
-
+	 	
 	importa_grafo(nos);
 	importa_arestas(grafo);
 
-	return 0;
-} 
+	vector<pair<long long, long long>> arestas_criticas = verifica_arestas_criticas(nos, grafo);
+	
+	ofstream arquivo("Pontos_criticos.csv");
+
+	arquivo << "lat1,long1,lat2,long2" << endl;
+
+
+	for (auto& i : arestas_criticas) { //Salva em um arquivo as coordenadas dos pontos que possuem um ponto critico entre eles no sentindo p1 --> p2
+		arquivo << fixed << setprecision(8) << nos[i.first].second << "," << nos[i.first].first << "," << nos[i.second].second <<"," << nos[i.second].first << endl;
+	}
+}
